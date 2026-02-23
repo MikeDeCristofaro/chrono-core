4832b1528e4f888f9c61ae1cf7e579ab287492cd
MikeDeCristofaro <mike.decristofaro@gmail.com>
2026-02-22 21:22:02 -0500

    Add RewindSnapshot struct for rewfeat: implement RewindSnapshot structind functionality

---
+++ Assets/Scripts/Rewind/RewindSnapshot.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace ChronoCore.Rewind
+{
+    public struct RewindSnapshot
+    {
+              public Vector2 Position;
+              public Vector2 Velocity;
+              public int Health;
+              public bool IsActive;
+              public int AnimatorStateHash;
+              public float AnimatorNormTime;
+              public float DashCooldownTimer;
+              public float IFrameTimer;
+              public float ChronoEnergyAmount;
+              public int CustomIntA;
+              public float CustomFloatA;
+              public bool CustomBoolA;
+    }
+}
